3b473e9667c65ebbd5250e93e828a7d92fa87b76
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-06-01 22:02:18 +0300

    can change qr code color

--- js/script.js
+++ js/script.js
@@ -1,21 +1,41 @@
-let input = document.querySelector("#input-url");
+let inputUrl = document.querySelector("#input-url");
+let inputColor = document.querySelector("select");
 const button = document.querySelector("#button-generate");
 let qrCodeDiv = document.querySelector("#qr-code-div");
+let qrCodeImage = document.querySelectorAll("#qr-code-div img");
 
 let qr;
 
 button.addEventListener("click",submit);
 
 function submit(){
-    let url = input.value;
-    console.log(url);
+    let url = inputUrl.value;
+    let color = inputColor.value;
 
-    if (qr == null) {
-        qr = new QRCode("qr-code-div",url);
+    if(qr == null || url == ""){
+        //qr = new QRCode("qr-code-div",url);
+        qr = new QRCode(qrCodeDiv,
+            {
+                text: url,
+                width: 128,
+                height: 128,
+	            colorDark : color,
+	            colorLight : "#ffffff",
+	            correctLevel : QRCode.CorrectLevel.H
+            }
+        );
     } 
     else{
-        qrCodeDiv.style.display = "none";
-        qr.makeCode(url);
-        qrCodeDiv.style.display = "block";
+        qrCodeDiv.innerHTML = "";
+        qr = new QRCode(qrCodeDiv,
+            {
+                text: url,
+                width: 128,
+                height: 128,
+	            colorDark : color,
+	            colorLight : "#ffffff",
+	            correctLevel : QRCode.CorrectLevel.H
+            }
+        );
     }
 }
